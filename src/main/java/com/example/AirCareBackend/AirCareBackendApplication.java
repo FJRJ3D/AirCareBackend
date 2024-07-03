2ccb10ed09f6beff154bec56c16ae590a87da291
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-07-03 18:05:45 +0200

    first commit

---
+++ src/main/java/com/example/AirCareBackend/AirCareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.AirCareBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirCareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirCareBackendApplication.class, args);
+	}
+
+}
